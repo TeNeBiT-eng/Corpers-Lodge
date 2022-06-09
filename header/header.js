fe3b45aba4c8dd313e65a85215df0f4aa99c7f3b
ddanny16 <dammydan16@gmail.com>
2022-06-09 12:21:43 +0200

    ...

---
+++ header/header.js
@@ -0,0 +1,15 @@
+ 
+// window.onscroll = function() {myFunction()};
+
+// window.onscroll = function() {myScrollIndicator()};
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+// function myScrollIndicator() {
+//   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   let scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
